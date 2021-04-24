b8fd72e73e16864b77340eab39078e845d455bde
emreboya <emreboya@users.noreply.github.com>
2021-04-24 11:25:31 +0300

    Delete meb-spring-csv2PGBD directory

--- meb-spring-csv2PGBD/src/main/java/com/meb/repo/csv2mongo/repository/CitizenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.meb.repo.csv2mongo.repository;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.meb.repo.csv2mongo.document.Citizens;
-
-public interface CitizenRepository extends MongoRepository<Citizens, String>{
-}
