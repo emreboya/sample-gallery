612252f93c838eed4a068eada455e0d4b3f98d00
emreboya <emreboya@gmail.com>
2021-04-24 11:06:40 +0300

    go

--- meb-spring-csv2PGBD/src/main/java/com/meb/repo/csv2PGDB/MebSpringCsv2PGDBApplication.java
+++ meb-spring-csv2PGBD/src/main/java/com/meb/repo/csv2PGDB/MebSpringCsv2PGDBApplication.java
@@ -1,13 +1,13 @@
-package com.meb.repo.csv2mongo;
+package com.meb.repo.csv2PGDB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MebSpringCsv2mongoApplication {
+public class MebSpringCsv2PGDBApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MebSpringCsv2mongoApplication.class, args);
+		SpringApplication.run(MebSpringCsv2PGDBApplication.class, args);
 	}
 
 }
