e50d6b88dfae50ca47ad3147d135974eb0671c87
emreboya <emreboya@gmail.com>
2021-04-26 07:34:10 +0300

    go
    go

--- meb-spring-persistency/src/main/java/com/meb/repo/model2db/controller/DownloadFromDB.java
+++ meb-spring-persistency/src/main/java/com/meb/repo/model2db/controller/DownloadFromDB.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.meb.repo.model2db.service.Csv2DatabaseService;
 
+// lest write our rest interface
+
 @RestController
 public class DownloadFromDB {
 	
