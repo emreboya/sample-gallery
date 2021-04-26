1b5ecbf18818bcb02bef1defd551a79b77ade350
emreboya <emreboya@gmail.com>
2021-04-26 07:38:24 +0300

    committed
    committed

--- meb-spring-persistency/src/main/java/com/meb/repo/model2db/controller/DownloadFromDB.java
+++ meb-spring-persistency/src/main/java/com/meb/repo/model2db/controller/DownloadFromDB.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.meb.repo.model2db.service.Csv2DatabaseService;
 
-// lest write our rest interface
+// rest interface shit
 
 @RestController
 public class DownloadFromDB {
